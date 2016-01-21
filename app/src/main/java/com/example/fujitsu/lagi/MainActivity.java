c6dc5edfbbade1284064a491fa943a277a4a8e42
kentangkiting <agustinus_bintang@yahoo.com>
2016-01-21 15:09:06 +0700

    <Bintang><21 jan 2016 15:08>

--- app/src/main/java/com/example/fujitsu/lagi/MainActivity.java
+++ app/src/main/java/com/example/fujitsu/lagi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fujitsu.lagi;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,13 +11,30 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.TextView;
+
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
 
 public class MainActivity extends AppCompatActivity {
     public static String EXTRA_MESSAGE = "com.example.fujitsu.lagi";
+    public static String EXTRA_MESSAGE2 = "com.example.fujitsu.lagi";
+    public static String apiUrl = "http://192.168.90.112:7001/Application14-RestTestLagi-context-root/jersey/test123";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,20 +79,122 @@ public class MainActivity extends AppCompatActivity {
         Intent intents = new Intent(this,BuatDisplayMessage.class);
         EditText test = (EditText)findViewById(R.id.editText3);
         String coba = test.getText().toString();
+//        String url = apiUrl;
+//        new callApi().execute(url);
         intents.putExtra(EXTRA_MESSAGE,coba);
         startActivity(intents);
 
     }
 
-    public void testInvoke(View view)
-    {
+    public void testInvoke (View view){
+        String url = apiUrl;
+        new callApi().execute(url);
 
+    }
+
+    private class callApi extends AsyncTask<String,String,String>{
+        private String content ;
+        TextView uiUpdate = (TextView)findViewById(R.id.textView2);
+        @Override
+        protected String doInBackground(String... params)
+        {
+
+            String urlString = params[0];
+            String resultToDisplay = "";
+
+//            InputStream is = null;
+//            storeStatus result = null;
+
+            try {
+                URL url = new URL(urlString);
+                URLConnection conn = url.openConnection();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                String line = null ;
+
+                while ((line = reader.readLine())!= null)
+                {
+                    sb.append(line+" ");
+                }
+                content = sb.toString();
+//                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+//                is  = urlConnection.getInputStream();
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+//            XmlPullParserFactory pullParserFactory;
+//
+//            try {
+//                pullParserFactory = XmlPullParserFactory.newInstance();
+//                XmlPullParser parser = pullParserFactory.newPullParser();
+//
+//                parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES,false);
+//                parser.setInput(is, null);
+//                result = parseXML(parser);
+//
+//            }
+//            catch (XmlPullParserException e){
+//                e.printStackTrace();
+//            }
+//            catch (IOException ie){
+//                ie.printStackTrace();
+//            }
+
+            return resultToDisplay;
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            uiUpdate.setText(content);
+        }
 
     }
 
-    public void invokeWS (RequestParams requestParams)
-    {
+//    private class storeStatus{
+//        public String statusNbr;
+//        public String hygieneResult;
+//
+//    }
 
-
-    }
+//    private storeStatus parseXML(XmlPullParser parser) throws XmlPullParserException, IOException
+//    {
+//        int eventType = parser.getEventType();
+//        storeStatus result = new storeStatus();
+//
+//        while( eventType!= XmlPullParser.END_DOCUMENT) {
+//            String name = null;
+//
+//            switch(eventType)
+//            {
+//                case XmlPullParser.START_TAG:
+//                    name = parser.getName();
+//
+//                    if( name.equals("Error")) {
+//                        System.out.println("Web API Error!");
+//                    }
+//                    else if ( name.equals("StatusNbr")) {
+//                        result.statusNbr = parser.nextText();
+//                    }
+//                    else if (name.equals("HygieneResult")) {
+//                        result.hygieneResult = parser.nextText();
+//                    }
+//
+//                    break;
+//
+//                case XmlPullParser.END_TAG:
+//                    break;
+//            } // end switch
+//
+//            eventType = parser.next();
+//        } // end while
+//
+//        return result;
+//
+//    }
 }
---
+++ app/src/main/java/com/example/fujitsu/lagi/WServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.fujitsu.lagi;
+
+/**
+ * Created by Fujitsu on 20/01/2016.
+ */
+public class WServiceTest {
+
+    private static String NAME_SPACE = "http://trs/";
+    private static String URL = "http://192.168.90.112:7001/Application11-WSCoba-context-root/WSTestPort?wsdl";
+    private static String SOAP_ACTION = "http://trs/";
+
+    public static String WSTest(String name, String webTest)
+    {
+        String res = null;
+        return res;
+
+    }
+}
